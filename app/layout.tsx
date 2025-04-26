ae74240e4b06e8dbcf992e5e47825f8a1d6f8776
ce <cecillemanalang@gmail.com>
2025-04-26 15:03:40 +0800

    feat: marketing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,38 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Question Card Game",
-  description: "A fun question card game to play with friends",
-  viewport: "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
-    generator: 'v0.dev'
-}
+  title: "Yet Another Strangers Card Game",
+  description: "A fun strangers card game to play with friends",
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" className="overscroll-none">
-      <body className={`${inter.className} overscroll-none touch-manipulation select-none fixed w-full h-full`}>
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </head>
+      <body
+        suppressHydrationWarning
+        className={`${inter.className} overscroll-none touch-manipulation select-none fixed w-full h-full`}
+      >
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,7 +1,16 @@
 "use client"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+import { useEffect, useState } from "react"
+import { ThemeProvider as NextThemeProvider } from "next-themes"
+
+export function ThemeProvider({ children, ...props }: any) {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
+
+  return <NextThemeProvider {...props}>{children}</NextThemeProvider>
 }
